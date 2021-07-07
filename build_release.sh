c3bc45961ce6aec7862961c649535bf68080df7c
Alissa Rao <lymia@lymiahugs.com>
2021-07-07 01:53:08 -0700

    Further formatting improvements to section hyperlinks.

--- build_release.sh
+++ build_release.sh
@@ -2,6 +2,7 @@
 
 cp "Luminous: the Dream.lyx" "LtD_Release.lyx"
 sed -i "s/usepackage\\[mark/usepackage[/g" "LtD_Release.lyx"
+sed -i "s/%%DRAFT%%//g" "LtD_Release.lyx"
 lyx "LtD_Release.lyx" -E pdf4 "LtD_Release_Temp.pdf"
 qpdf "LtD_Release_Temp.pdf" "LtD_Release.pdf" \
     --compress-streams=n --object-streams=generate --coalesce-contents --optimize-images
