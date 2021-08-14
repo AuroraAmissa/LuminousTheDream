437986e9529e78a5acbcb855f0f03221d87d429d
Alissa Rao <lymia@lymiahugs.com>
2021-08-14 13:15:56 -0700

    Begin work on a character sheet, further core book work.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ echo "Building Luminous distribution..."
 echo " - Creating directory structure"
 rm -rf build || exit 1
 mkdir -p build/out/"Bonus Files" || exit 1
+mkdir -p build/out/"Character Sheets" || exit 1
 mkdir -p build/tex || exit 1
 mkdir -p dist || exit 1
 
@@ -21,7 +22,7 @@ sed -i -e '/\\branch .*/,+1s/\\selected.*/\\selected 0/' build/*.lyx || exit 1
 activate_branch() {
     sed -i -e '/\\branch '$2'.*/,+1s/\\selected.*/\\selected 1/' "build/$1.lyx" || exit 1
 }
-render_luminous() {
+render_pdf() {
     # Creates the direct output PDF
     lyx -v "build/$1.lyx" -E pdf4 "build/$1_Temp.pdf" || exit 1
     
@@ -30,7 +31,7 @@ render_luminous() {
     qpdf "build/$1_Temp.pdf" "build/$1.pdf" \
         --compress-streams=y --object-streams=generate --coalesce-contents \
         --encrypt "" "pls dont do anything weird with the password :( :(" 128 \
-        --extract=y --assemble=n --annotate=n --form=n --modify-other=n --print=full --modify=none --cleartext-metadata -- || exit 1
+        --extract=y --assemble=n --form=n --annotate=n --modify-other=n --print=full --modify=none --cleartext-metadata "${@:4}" -- || exit 1
     cp "build/$1.pdf" "build/out/$2$3.pdf" || exit 1
     
     # Build an epub
@@ -45,6 +46,18 @@ create_archive() {
     zip -r "$1.zip" * || exit 1
     mv "$1.zip" ../../dist || exit 1
     cd ../..
+}
+create_source_archive() {
+    mkdir "build/LuminousSources-$VERSION" || exit 1
+    cp -r fonts/ includes/ build.sh *.md *.lyx "build/LuminousSources-$VERSION" || exit 1
+    rm "build/LuminousSources-$VERSION/includes"/*.xcf || exit 1
+    mkdir "build/LuminousSources-$VERSION/.git" || exit 1
+    cp -r .git/gitHeadInfo.gin "build/LuminousSources-$VERSION/.git" || exit 1
+
+    cd build
+    tar --xz -cvf "$1.tar.xz" "LuminousSources-$VERSION" || exit 1
+    mv "$1.tar.xz" ../dist || exit 1
+    cd ..
 }
 
 VERSION="$(git describe --tags --long --always --match '[0-9]*.*')"
@@ -76,6 +89,8 @@ ci)
 ;;
 esac
 
-render_luminous Book_Luminous "" "Luminous the Dream$FILE_MAKER" || exit 1
-render_luminous Book_Luminous_Comm "Bonus Files/" "Luminous the Dream$FILE_MARKER (Author Commentary)" || exit 1
+render_pdf Book_Luminous "" "Luminous the Dream$FILE_MAKER" || exit 1
+render_pdf Book_Luminous_Comm "Bonus Files/" "Luminous the Dream$FILE_MARKER (Author Commentary)" || exit 1
+render_pdf Sheet_Luminous "Character Sheets/" "Luminous" --assemble=y --form=y --annotate=y || exit 1
 create_archive "Luminous the Dream$ZIP_FILE_BIND $ZVERSION" || exit 1
+create_source_archive "LuminousSources$ZIP_FILE_BIND $ZVERSION" || exit 1
