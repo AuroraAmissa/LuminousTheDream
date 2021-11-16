7a38a441301a4cf2791be22043e70b7706c69fb4
Alissa Rao <lymia@lymiahugs.com>
2021-11-16 07:49:32 -0800

    Split the old Luminous document into seperate Maou and Luminous books.

--- build.sh
+++ build.sh
@@ -10,8 +10,15 @@ echo " - Creating Luminous-specific directories..."
 mkdir -p build/out/"Bonus Files" || exit 1
 mkdir -p build/out/"Character Sheets" || exit 1
 
+# Luminous
 render_pdf Book_Luminous "" "Luminous the Dream$FILE_MAKER" || exit 1
 render_pdf Book_Luminous_Comm "Bonus Files/" "Luminous the Dream$FILE_MARKER (Author Commentary)" || exit 1
 render_pdf Sheet_Luminous "Character Sheets/" "Luminous" --assemble=y --form=y --annotate=y || exit 1
+
+# Maou
+render_pdf Book_Maou "" "Maou the Lineage$FILE_MAKER" || exit 1
+render_pdf Book_Maou_Comm "Bonus Files/" "Maou the Lineage$FILE_MARKER (Author Commentary)" || exit 1
+
+# Build archives
 create_archive "Luminous the Dream$ZIP_FILE_BIND $ZVERSION" || exit 1
 create_source_archive "LuminousSources$ZIP_FILE_BIND $ZVERSION" || exit 1
