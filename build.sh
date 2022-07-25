8db67b708e75a8a6a4b565131c4b6ddb59e0ba7e
Alissa Rao <lymia@lymiahugs.com>
2022-07-25 14:50:39 -0700

    Remove author commentary version - I think I'll make a seperate addon document for that.

--- build.sh
+++ build.sh
@@ -12,12 +12,10 @@ mkdir -p build/out/"Character Sheets" || exit 1
 
 # Luminous
 render_pdf Book_Luminous "" "Luminous the Dream$FILE_MAKER" || exit 1
-render_pdf Book_Luminous_Comm "Bonus Files/" "Luminous the Dream$FILE_MARKER (Author Commentary)" || exit 1
 render_pdf Sheet_Luminous "Character Sheets/" "Luminous" --assemble=y --form=y --annotate=y || exit 1
 
 # Maou
 #render_pdf Book_Maou "" "Maou the Lineage$FILE_MAKER" || exit 1
-#render_pdf Book_Maou_Comm "Bonus Files/" "Maou the Lineage$FILE_MARKER (Author Commentary)" || exit 1
 
 # Build archives
 create_archive "Luminous the Dream$ZIP_FILE_BIND $ZVERSION" || exit 1
