e1dd4ecc78b6105cad95e88aac1b34c98a47b59e
Alissa Rao <lymia@lymiahugs.com>
2022-07-25 14:58:50 -0700

    Split off crossover material into its own book.

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ echo "Building Luminous distribution..."
 init_build $1 || exit 1
 
 echo " - Creating Luminous-specific directories..."
-mkdir -p build/out/"Bonus Files" || exit 1
 mkdir -p build/out/"Character Sheets" || exit 1
 
 # Luminous
 render_pdf Book_Luminous "" "Luminous the Dream$FILE_MAKER" || exit 1
+render_pdf Book_Luminous_Crossover "" "Luminous the Dream - Crossing Paths$FILE_MAKER" || exit 1
 render_pdf Sheet_Luminous "Character Sheets/" "Luminous" --assemble=y --form=y --annotate=y || exit 1
 
 # Maou
